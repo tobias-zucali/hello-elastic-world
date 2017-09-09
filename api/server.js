a2bc60848b863869da559fcef771137b7432b555
Tobias Zucali <tzuc@usersnap.com>
2017-09-09 21:19:52 +0200

    add postgres db | enable remote debugging for app_api

--- api/server.js
+++ api/server.js
@@ -1,14 +1,26 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+
+const { Client, Pool } = require('pg');
+
+const pool = new Pool()
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+});
+
+console.log('hi there!')
+
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World (/)!')
-})
+  res.send('Hello World (/)!');
+});
 
 app.get('/api', function (req, res) {
-  res.send('Hello World (/api)!')
-})
+  res.send('Hello World (/api)!');
+});
 
 app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
-})
+  console.log('Example app listening on port 3001!');
+});
