f77afd81714363274f4bbf50b4e078abce039213
Tobias Zucali <tzuc@usersnap.com>
2017-09-10 22:24:35 +0200

    add postgres

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,28 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
+  dialect: 'postgres'
+});
+
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync()
+  .then(
+    () => User.findOrCreate({where: {
+      username: 'janedoe',
+      birthday: new Date(1980, 6, 20)
+    }})
+    .spread(jane => {
+      console.log(jane.get({
+        plain: true
+      }));
+    })
+  );
+
 var express = require('express')
 var app = express()
 
